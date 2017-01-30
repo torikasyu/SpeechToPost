59769fefefc8390ee44db7b3ac4cee123df64f0f
torikasyu <tanaka@lyles.jp>
2017-01-30 21:23:06 +0900

    First Commit

---
+++ SpeechToPost/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeechToPost
+//
+//  Created by TANAKAHiroki on 2017/01/30.
+//  Copyright © 2017年 torikasyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

fa349bc8572d4650a144ff87e4153352dc5a7a15
torikasyu <tanaka@lyles.jp>
2017-01-30 23:25:52 +0900

    WatchKit

--- SpeechToPost WatchKit Extension/InterfaceController.swift
+++ SpeechToPost WatchKit Extension/InterfaceController.swift
@@ -11,7 +11,20 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
+    @IBOutlet var lblTest: WKInterfaceLabel!
 
+    @IBAction func btnInput() {
+        //入力画面を開く
+        presentTextInputController(
+            withSuggestions: ["あつい", "さむい"],     //第一引数（suggestions）
+            allowedInputMode: .plain,    //第二引数（allowedInputMode）
+            completion: { (str) -> Void in    //第三引数（completion）
+                if str != nil {
+                    self.lblTest.setText("\(str)")
+                }
+        }
+        )
+    }
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
@@ -21,6 +34,8 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
+        self.lblTest.setText("ほげほげ")
     }
     
     override func didDeactivate() {
